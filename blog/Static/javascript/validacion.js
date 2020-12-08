9673cda352ba1ac3c343abc32192a2fea619c29a
VelasquezDaniel <velasquezdaniel@uninorte.edu.co>
2020-12-08 15:40:34 -0500

    actualizacion

--- blog/Static/javascript/validacion.js
+++ blog/Static/javascript/validacion.js
@@ -1,30 +1,24 @@
-var intentos=3;
-function validar_formulario(){
+var intentos = 3;
 
- var nombre=document.getElementById("login-usuario").value;
- var clave=document.getElementById("login-contraseña").value;
- if(nombre=="user" && clave=="user")
- {
-    location.replace("Dashboard.html");
- }
- else
- {
-  if(intentos==0)
-  {
-    alert("Se han acabado los intentos");
-  }
-  else
-  {
-    intentos=intentos-1;
-   alert("Intentos restantes: "+intentos);
-   if(intentos==0)
-   {
-    document.getElementById("usuario").disabled=true;
-    document.getElementById("password").disabled=true;
-    document.getElementById("submit").disabled=true;
-   }
-  }
- }
- 
- return false;
+function validar_formulario() {
+
+    var nombre = document.getElementById("usuario").value;
+    var clave = document.getElementById("password").value;
+    if ((nombre == "usuario" && clave == "user") || (nombre == "admin" && clave == "admin")) {
+        location.replace("dashboard.html");
+    } else {
+        if (intentos == 0) {
+            alert("Se han acabado los intentos");
+        } else {
+            intentos = intentos - 1;
+            alert("Intentos restantes: " + intentos);
+            if (intentos == 0) {
+                document.getElementById("usuario").disabled = true;
+                document.getElementById("password").disabled = true;
+                document.getElementById("submit").disabled = true;
+            }
+        }
+    }
+
+    return false;
 }
--- blog/app.py
+++ blog/app.py
@@ -12,6 +12,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/perfil')
+def userInf():
+    return render_template('userInformation.html')
+
 @app.route('/CrearCuenta')
 def registro():
     return render_template('createUser.html')
@@ -20,13 +24,36 @@ def registro():
 def forgetPassword():
     return render_template('forgetPassword.html')
     
+@app.route('/cambiarClave')
+def changePassword():
+    return render_template('changePassword.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
 @app.route('/create')
 def createBlog():
     return render_template('createBlog.html')
+
+@app.route('/edit', methods=['POST'])
+def editBlog():
+    return render_template('editBlog.html')
 
 @app.route('/resultados', methods=['GET'])
 def search():
     return render_template('search.html')
 
+@app.route('/validacion', methods=['POST'])
+def validacion():
+    print(request.method)
+    usuario = request.form['usuario']
+    password = request.form['password']
+    if usuario == "admin" and password == "admin":
+        return render_template('dashboard.html')
+    else:
+        flash("Usuario y/o contraseña invalido")
+        return render_template('login.html')
+    
 if __name__ == '__main__':
     app.run()
