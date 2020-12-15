b8a82849d41bb1ff3cb1761b6d41b42b5701ff50
VelasquezDaniel <velasquezdaniel@uninorte.edu.co>
2020-12-14 21:57:21 -0500

    Co-authored-by: Santiago Borbón <santiagoborbon@hotmail.es>

--- blog/app.py
+++ blog/app.py
@@ -1,5 +1,3 @@
-#import yagmail as yagmail
-
 import pdfkit #para pdf
 #path_wkhtmltopdf = 'venv\\include\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'
 #config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
@@ -12,7 +10,7 @@ import utils
 from db import get_db, close_db
 import os
 from sqlite3 import Error
-
+import yagmail as yagmail
 
 
 app = Flask(__name__)
@@ -55,6 +53,9 @@ def validacion():
                 session.clear()
                 session['usuario_ID'] = user[0]
                 session['username'] = user[1]
+                session['correo'] = user[3]
+                session['nombre'] = user[4]
+                session['apellido'] = user[5]
                 return redirect(url_for('dashboard'))
             flash( error )
         return render_template( 'login.html' )    
@@ -140,13 +141,37 @@ def registro():
 
 @app.route('/recuperarCuenta')
 def forgetPassword():
-    return render_template('forgetPassword.html')
-    
+    return render_template('forgetPassword')
+
+
+
+@app.route('/sendEmail', methods=('GET', 'POST'))
+def sendEmail():
+    email = request.form['email']
+    db = get_db() #Conectarse a la base de datos
+
+    if not utils.isEmailValid( email ):
+                error = 'Correo invalido'
+                flash( error )
+                return render_template( 'register.html' )
+
+
+    #Preguntar si el correo no ha sido registrado anteriormente
+    if db.execute( 'SELECT id FROM usuarios WHERE correo = ?', (email,) ).fetchone() is not None:
+        yag = yagmail.SMTP('micuenta@gmail.com', 'clave') #modificar con tu informacion personal
+        yag.send(to=email, subject='Recupera tu cuenta',
+        contents='Bienvenido, usa este link para entrar a tu cuenta ')
+    return render_template('forgetPassword')
+
+
+
+
 @app.route('/cambiarClave')
 @login_required
 def changePassword():
     return render_template('changePassword.html')
 
+    
 @app.route('/dashboard')
 @login_required
 def dashboard():
@@ -155,21 +180,28 @@ def dashboard():
 @app.route('/create')
 @login_required
 def createBlog():
-    #try:
+    return render_template( 'createBlog.html' )
+
+@app.route('/createBlog')
+@login_required
+def crearBlog():
+    try:
         if request.method == 'POST':
             titulo = request.form['titulo']
             cuerpo = request.form['cuerpo']
             imagen = "No hay" #DEBEMOS MODIFICAR ESTO
             etiquetas = "Modificar etiquetas"
-            usuarioCreador = 1  #session['usuario_ID']
+            usuarioCreador = session['usuario_ID']
             likes = 0
             fechaCreacion = datetime.now()
+            error = None
+            db = get_db() #Conectarse a la base de datos
+
             if request.form['privacidad'] == True:
                 privado = True
             else:
                 privado = False 
-            error = None
-            db = get_db() #Conectarse a la base de datos
+            
             if cuerpo is None:
                 error = "debe ingresar el titulo del blog"
                 flash( error )
@@ -197,15 +229,8 @@ def createBlog():
             #flash( 'Revisa tu correo para activar tu cuenta' )
             return render_template( 'dashboard.html', blog_created="El blog ha sido creado con exito" )
         return render_template( 'createBlog.html' )
-    #except:
-        #return render_template( 'createBlog.html' )       
-
-@app.route('/edit', methods=['POST'])
-@login_required
-def editBlog():
-    return render_template('editBlog.html')
-
-@app.route('/resultados', methods=['GET'])
+    except:
+        return render_template( 'createBlog.html' )       
 def search():
     return render_template('search.html')
 
@@ -213,19 +238,3 @@ def search():
 def load_logged_in_user():
     user_id = session.get( 'usuario_ID' )
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM usuarios WHERE usuario_ID = ?', (user_id,)
-        ).fetchone()
-
-
-@app.route( '/logout' )
-def logout():
-    session.clear()
-    return redirect( url_for( 'login' ) )
-
-
-if __name__ == '__main__':
-    app.run(debug=True,port=5000)
